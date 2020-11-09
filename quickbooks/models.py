29beea424e973715c867152e684be2614e37fbf0
cbatie <56029970+cbatie@users.noreply.github.com>
2020-11-09 21:51:00 +0000

    Update models.py

--- quickbooks/models.py
+++ quickbooks/models.py
@@ -70,3 +70,8 @@ class TextAnsibleMigrationsTry(models.Model):
     ansible_test = models.CharField(max_length=250, primary_key=True, unique=True,
                                   default=generate_sso_token)
     test_testing = models.ForeignKey(CorporateInformation, on_delete=models.CASCADE, related_name="testing")
+    
+    testing = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return "%s %s" % (self.testing, self.test_testing)
